823667aa8d08f5d2d9ae6b4fd81731ca275d6a20
Miles Egan <miles@plastaq.com>
2014-11-07 14:05:01 +0700

    Fix scaling, margins and axes.

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -11,33 +11,56 @@ var Chart = React.createClass({
     d3.destroy(el);
   },
   render() {
-    return (<svg width='200' height='200' className="chart"></svg>);
+    return (<svg height='400' className="chart col-md-12"></svg>);
   },
   shouldComponentUpdate() {
     this.updateChart();
     return false;
   },
   updateChart() {
-    var c = d3.select(this.getDOMNode());
+    var node = this.getDOMNode();
+    node.innerHTML = "";
+    var margin = 40;
+    var box = node.getBoundingClientRect();
+    var maxX = d3.max(this.props.data, d => d.x);
+    var maxY = d3.max(this.props.data, d => d.y);
+    var xScale = d3.scale.linear().domain([0, maxX]).range([margin, box.width - margin]);
+    var yScale = d3.scale.linear().domain([0, maxY]).range([box.height - margin, margin]);
+    var c = d3.select(node);
+    var xAxis = d3.svg.axis()
+      .scale(xScale);
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .ticks(10)
+      .orient('left');
     var lineFunc = d3.svg.line()
-      .x(d => d.x * 10)
-      .y(d => d.y * 5)
+      .x(d => xScale(d.x))
+      .y(d => yScale(d.y))
       .interpolate('linear');
     c.append('path')
       .attr('d', lineFunc(this.props.data))
       .attr('stroke', 'blue')
-      .attr('stroke-width', 2)
+      .attr('stroke-width', 1)
       .attr('fill', 'none');
+    c.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${box.height - margin})`)
+      .call(xAxis);
+    c.append('g')
+      .attr('class', 'y axis')
+      .attr('transform', `translate(${margin},0)`)
+      .call(yAxis);
   }
 });
 
 var HNTrendUI = React.createClass({
   getInitialState() {
     var sampleData = [
+      {x: 0, y: 1},
       {x: 1, y: 2},
       {x: 2, y: 20},
       {x: 3, y: 10},
-      {x: 4, y: 4},
+      {x: 4, y: 4}
     ];
     return {
       data: sampleData,
@@ -45,15 +68,14 @@ var HNTrendUI = React.createClass({
     }
   },
   componentDidMount() {
-    $.get("http://hn.globalonset.com/api/v0/topic/apple", null, (data) => {
-      this.setState({ topics: [data] });
-    });
+    $.get("http://hn.globalonset.com/api/v0/topic/apple",
+      null,
+      (data) => { this.setState({topics: [data]}); });
     console.log('component mounted');
   },
   render() {
     return (
-      <div class='row'>
-        <h1>hn trends</h1>
+      <div className='row'>
         <Chart data={this.state.data} domain={this.state.domain}/>
       </div>
     );
